4c0ab5e9d650e420e42c41f1c464e374b70acd20
Talha Javed <talhajaved020993@gmail.com>
2019-05-13 23:48:34 +0500

    This is a new commit for what I originally planned to be amended

---
+++ Astronomy/Astronomy/Pages/PersonalizePage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Astronomy.Pages
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class PersonalizePage : ContentPage
+	{
+		public PersonalizePage ()
+		{
+			InitializeComponent ();
+
+            btnSave.Clicked += BtnSaveClicked;
+            btnCancel.Clicked += BtnCancelClicked;
+		}
+
+        async void BtnCancelClicked(object sender, EventArgs e)
+        {
+            await this.Navigation.PopModalAsync();
+        }
+
+        async void BtnSaveClicked(object sender, EventArgs e)
+        {
+            Application.Current.Properties["Name"] = entryName.Text;
+            Application.Current.Properties["Icon"] = pickerIcon.SelectedItem as string;
+
+
+            await Application.Current.SavePropertiesAsync();
+
+            await this.Navigation.PopModalAsync()
+        }
+
+    }
+}
